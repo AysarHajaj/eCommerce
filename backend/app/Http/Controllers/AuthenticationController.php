6a1fe7c0e46b6855409a0b7ff5ac0208fb4d3493
aysar <aysarhajaj@gmail.com>
2022-12-08 18:57:53 +0400

    add AuthenticationController and routes

---
+++ backend/app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+
+        $response = [];
+        $response['token'] = $user->createToken('eCommerce')->accessToken;
+
+        return response()->json([$response], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $input = $request->all();
+
+        if (Auth::attempt($input)) {
+            $user = Auth::user();
+            $response = [];
+            $response['token'] = $user->createToken('eCommerce')->accessToken;
+
+            return response()->json([$response], 200);
+        }
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+        $user->token()->revoke();
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [AuthenticationController::class, 'register']);
+Route::post('login', [AuthenticationController::class, 'login']);
+Route::middleware("auth:api")->get('logout', [AuthenticationController::class, 'logout']);
+Route::middleware("auth:api")->get('user', function (Request $request) {
+    return response()->json(Auth::user());
 });
