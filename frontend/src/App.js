37d333142e1c151a9225081d170a1375558a7354
aysar <aysarhajaj@gmail.com>
2022-12-09 11:53:51 +0400

    remove unwanted code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
-import Button from "@mui/material/Button";
 import Layout from "./root/layout/Layout";
 
 function App() {
