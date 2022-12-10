2cc8c19e901d2706443085df88f7e225229c6a40
aysar <aysarhajaj@gmail.com>
2022-12-11 03:38:13 +0400

    remove middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ChildCategoryController;
 use App\Http\Controllers\SubCategoryController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,7 +21,7 @@ Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
 Route::middleware("auth:api")->get('logout', [AuthenticationController::class, 'logout']);
 Route::get('unauthenticated', [AuthenticationController::class, 'unauthenticated'])->name('unauthenticated');
-Route::resource('categories', CategoryController::class)->middleware("auth:api");
+Route::resource('categories', CategoryController::class);
 Route::post('categories/{id}', [CategoryController::class, 'update']);
-Route::resource('sub_categories', SubCategoryController::class)->middleware("auth:api");
-Route::resource('child_categories', ChildCategoryController::class)->middleware("auth:api");
+Route::resource('sub_categories', SubCategoryController::class);
+Route::resource('child_categories', ChildCategoryController::class);
