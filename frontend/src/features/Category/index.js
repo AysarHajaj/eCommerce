e34c4ab6e089eed7ba06e4b219d17424e1a17ba5
aysar <aysarhajaj@gmail.com>
2022-12-10 20:12:50 +0400

    add data from store to category component

--- frontend/src/features/Category/index.js
+++ frontend/src/features/Category/index.js
@@ -1,5 +1,84 @@
-import React from "react";
+import React, { useEffect, useMemo, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { DataGrid } from "@mui/x-data-grid";
+import EditIcon from "@mui/icons-material/Edit";
+import IconButton from "@mui/material/IconButton";
+import AppSwitch from "../../app/components/AppSwitch";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import {
+  getCategories,
+  selectGetCategories,
+  deleteCategory,
+  selectDeleteCategories,
+} from "./categorySlice";
 
-export default () => {
-  return <div>Category</div>;
+const Category = () => {
+  const dispatch = useDispatch();
+  const { data } = useSelector(selectGetCategories);
+
+  const handleDelete = useCallback((id) => {
+    dispatch(deleteCategory(id));
+  });
+
+  const columns = useMemo(() => {
+    return [
+      { field: "id", headerName: "ID", width: 70 },
+      { field: "image", headerName: "Image", width: 130 },
+      { field: "name", headerName: "Name", width: 130 },
+      {
+        field: "status",
+        headerName: "Status",
+        width: 130,
+        renderCell: (params) => {
+          const changeStatus = (checked) => {
+            console.log("from category log checked", checked, params.row.id);
+          };
+          return (
+            <AppSwitch
+              isActive={!params.row.deactivated_at}
+              changeStatus={changeStatus}
+            />
+          );
+        },
+      },
+      {
+        field: "action",
+        headerName: "Action",
+        width: 100,
+        renderCell: (params) => {
+          return (
+            <React.Fragment>
+              <IconButton>
+                <EditIcon />
+              </IconButton>
+              <IconButton onClick={() => handleDelete(params.row.id)}>
+                <DeleteIcon />
+              </IconButton>
+            </React.Fragment>
+          );
+        },
+      },
+    ];
+  }, []);
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Add
+      </Button>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+      />
+    </div>
+  );
 };
+
+export default React.memo(Category);
