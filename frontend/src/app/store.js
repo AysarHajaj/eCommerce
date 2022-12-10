6004743ece2251188e0cd639ba8d7d591ec940ef
aysar <aysarhajaj@gmail.com>
2022-12-10 22:28:13 +0400

    add sub category store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import categoryReducer from "../features/Category/categorySlice";
+import subCategoryReducer from "../features/SubCategory/subCategorySlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     category: categoryReducer,
+    sub_category: subCategoryReducer,
   },
 });
