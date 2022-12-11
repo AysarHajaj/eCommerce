761607c07ba73ecdf0a0855d7f48beccafbd3f73
aysar <aysarhajaj@gmail.com>
2022-12-12 03:23:56 +0400

    add routes

--- frontend/src/constant/index.js
+++ frontend/src/constant/index.js
@@ -3,4 +3,14 @@ export default {
     admin: 1,
     vendor: 2,
   },
+  routes: {
+    DASHBOARD: {
+      path: "/",
+      label: "Dashboard",
+    },
+    CATEGORY: {
+      path: "/category",
+      label: "Category",
+    },
+  },
 };
