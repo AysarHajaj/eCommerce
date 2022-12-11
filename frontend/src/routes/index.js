f58be28dc004b07e3eb15ff9786f2c68b7d7e50b
aysar <aysarhajaj@gmail.com>
2022-12-12 03:21:31 +0400

    add routes path  from constant file

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,22 +1,42 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import constant from "../constant";
 
 const Dashboard = React.lazy(() => import("../features/Dashboard"));
 const Category = React.lazy(() => import("../features/Category"));
 const SubCategory = React.lazy(() => import("../features/SubCategory"));
 const ChildCategory = React.lazy(() => import("../features/ChildCategory"));
+const CategoryForm = React.lazy(() =>
+  import("../features/Category/components/Form")
+);
 
 export default () => {
   return (
     <Routes>
-      <Route element={<Dashboard />} path="/" />
+      <Route element={<Dashboard />} path={constant.routes.DASHBOARD.path} />
       <Route
         element={
           <React.Suspense fallback="Loading...">
             <Category />
           </React.Suspense>
         }
-        path="/category"
+        path={constant.routes.CATEGORY.path}
+      />
+      <Route
+        element={
+          <React.Suspense fallback="Loading...">
+            <CategoryForm />
+          </React.Suspense>
+        }
+        path="/category/create"
+      />
+      <Route
+        element={
+          <React.Suspense fallback="Loading...">
+            <CategoryForm />
+          </React.Suspense>
+        }
+        path="/category/edit/:id"
       />
       <Route
         element={
