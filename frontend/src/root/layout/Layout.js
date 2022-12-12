c1c584f6c00303e2cfa91b3d6a9888c92e73e5c8
aysar <aysarhajaj@gmail.com>
2022-12-12 11:09:59 +0400

    add auth condition

--- frontend/src/root/layout/Layout.js
+++ frontend/src/root/layout/Layout.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Navigate } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -6,10 +7,15 @@ import AppBar from "./AppBar";
 import DrawerApp from "./AppDrawer";
 import AppMain from "./AppMain";
 import AppRoutes from "../../routes";
+import { selectLogin } from "../../features/Login/loginSlice";
+import { useSelector } from "react-redux";
+import constant from "../../constant";
 
 const Layout = () => {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
+  const { data } = useSelector(selectLogin);
+  const isAuthenticated = !!data?.token;
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -28,8 +34,12 @@ const Layout = () => {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar open={open} handleDrawerOpen={handleDrawerOpen} />
-      <DrawerApp open={open} handleDrawerClose={handleDrawerClose} />
+      {isAuthenticated && (
+        <AppBar open={open} handleDrawerOpen={handleDrawerOpen} />
+      )}
+      {isAuthenticated && (
+        <DrawerApp open={open} handleDrawerClose={handleDrawerClose} />
+      )}
       <AppMain open={open}>
         <AppRoutes />
       </AppMain>
