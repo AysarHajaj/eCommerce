b32d8a6090d198550db7b29bfc47775a8c132ff5
aysar <aysarhajaj@gmail.com>
2022-12-13 21:42:05 +0400

    fix  sub category component

--- frontend/src/features/SubCategory/subCategorySlice.js
+++ frontend/src/features/SubCategory/subCategorySlice.js
@@ -4,42 +4,44 @@ import api from "../../api";
 const initialState = {
   get: {
     data: [],
-    isFetched: false,
+    isLoading: false,
     error: null,
   },
   delete: {
     data: "",
-    isFetched: false,
+    isLoading: false,
     error: null,
   },
   change_status: {
     data: "",
-    isFetched: false,
+    isLoading: false,
     error: null,
   },
 };
 
-export const getSubCategories = createAsyncThunk("sub_categories/get", () =>
-  api
-    .getSubCategories()
-    .then((response) => response.data)
-    .catch((error) => error)
+export const getSubCategories = createAsyncThunk(
+  "sub_categories/get",
+  (data = null, { rejectWithValue }) =>
+    api
+      .getSubCategories()
+      .then((response) => response.data)
+      .catch((error) => rejectWithValue(error?.response?.data))
 );
 export const deleteSubCategory = createAsyncThunk(
   "sub_categories/delete",
-  (id) =>
+  (id, { rejectWithValue }) =>
     api
       .deleteSubCategory(id)
       .then((response) => ({ data: response.data, id }))
-      .catch((error) => error)
+      .catch((error) => rejectWithValue(error?.response?.data))
 );
 export const changeSubCategoryStatus = createAsyncThunk(
   "sub_categories/change_status",
-  (id) =>
+  (id, { rejectWithValue }) =>
     api
       .changeSubCategoryStatus(id)
       .then((response) => ({ data: response.data, id }))
-      .catch((error) => error)
+      .catch((error) => rejectWithValue(error?.response?.data))
 );
 
 export const subCategorySlice = createSlice({
@@ -50,42 +52,42 @@ export const subCategorySlice = createSlice({
     builder
       //get sub_categories case
       .addCase(getSubCategories.pending, (state) => {
-        state.get.isFetched = false;
+        state.get.isLoading = true;
         state.get.error = null;
       })
       .addCase(getSubCategories.fulfilled, (state, action) => {
-        state.get.isFetched = true;
+        state.get.isLoading = false;
         state.get.data = action.payload.data;
       })
       .addCase(getSubCategories.rejected, (state, action) => {
-        state.get.isFetched = true;
+        state.get.isLoading = false;
         state.get.error = action.payload;
       })
       //delete sub_categories case
 
       .addCase(deleteSubCategory.pending, (state) => {
-        state.delete.isFetched = false;
+        state.delete.isLoading = true;
         state.delete.error = null;
       })
       .addCase(deleteSubCategory.fulfilled, (state, action) => {
-        state.delete.isFetched = true;
+        state.delete.isLoading = false;
         state.delete.data = action.payload.data;
         state.get.data = state.get.data.filter(
           (subCategory) => subCategory.id != action.payload.id
         );
       })
       .addCase(deleteSubCategory.rejected, (state, action) => {
-        state.delete.isFetched = true;
+        state.delete.isLoading = false;
         state.delete.error = action.payload;
       })
       // change category status case
 
       .addCase(changeSubCategoryStatus.pending, (state) => {
-        state.change_status.isFetched = false;
+        state.change_status.isLoading = true;
         state.change_status.error = null;
       })
       .addCase(changeSubCategoryStatus.fulfilled, (state, action) => {
-        state.change_status.isFetched = true;
+        state.change_status.isLoading = false;
         state.change_status.data = action.payload.data;
         state.get.data = state.get.data.map((subCategory) => {
           if (subCategory.id === action.payload.id) {
@@ -95,7 +97,7 @@ export const subCategorySlice = createSlice({
         });
       })
       .addCase(changeSubCategoryStatus.rejected, (state, action) => {
-        state.change_status.isFetched = true;
+        state.change_status.isLoading = false;
         state.change_status.error = action.payload;
       });
   },
