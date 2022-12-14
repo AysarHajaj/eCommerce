077cc3302ee1e77d65a4c62a7a70a983efb5d910
aysar <aysarhajaj@gmail.com>
2022-12-14 13:09:33 +0400

    make image  nullable

--- backend/database/migrations/2022_12_08_151555_create_categories_table.php
+++ backend/database/migrations/2022_12_08_151555_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamp('deactivated_at')->nullable();
             $table->softDeletes();
             $table->timestamps();
