e2c2ac992294785c24606b3d6071b556c13c5762
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2022-12-14 20:41:40 +0200

    Show user info in the app bar and enable logout

--- frontend/src/root/layout/AppBar.js
+++ frontend/src/root/layout/AppBar.js
@@ -1,12 +1,23 @@
 import React, { useMemo } from "react";
 import { styled } from "@mui/material/styles";
+import { useLocation, useNavigate } from "react-router-dom";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
-import { useLocation } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import Tooltip from "@mui/material/Tooltip";
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import MenuItem from "@mui/material/MenuItem";
 import constant from "../../constant";
+import useAuth from "../../hooks/useAuth";
+import Divider from "@mui/material/Divider";
 
 const drawerWidth = 240;
 
@@ -27,8 +38,27 @@ const AppBarStyle = styled(MuiAppBar, {
   }),
 }));
 
+const settings = {
+  PROFILE: {
+    label: "Profile",
+    icon: <PersonIcon />,
+    to: "/settings/profile",
+  },
+  LOGOUT: {
+    label: "Logout",
+    icon: <LogoutRoundedIcon />,
+    to: "/logout",
+  },
+};
+
 const AppBar = ({ open, handleDrawerOpen }) => {
   const location = useLocation();
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
+
+  const {
+    auth: { user },
+  } = useAuth();
   const pageName = useMemo(() => {
     return (
       Object.values(constant.ROUTES).find((route) => {
@@ -36,6 +66,16 @@ const AppBar = ({ open, handleDrawerOpen }) => {
       })?.label || "Not Found"
     );
   }, [location.pathname]);
+
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <AppBarStyle position="fixed" open={open}>
@@ -49,9 +89,84 @@ const AppBar = ({ open, handleDrawerOpen }) => {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
+        <Typography
+          color="white"
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ flexGrow: 1 }}
+        >
           {pageName}
         </Typography>
+
+        <Box display="flex" sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton
+              disableRipple
+              onClick={handleOpenUserMenu}
+              sx={{ p: 0 }}
+            >
+              <Avatar alt={user?.name} src={user?.image} />
+              <Typography
+                color="white"
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ ml: "10px" }}
+                textTransform="capitalize"
+              >
+                {user?.name}
+              </Typography>
+              {Boolean(anchorElUser) ? (
+                <ArrowDropUpIcon htmlColor="white" />
+              ) : (
+                <ArrowDropDownIcon htmlColor="white" />
+              )}
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            MenuListProps={{
+              style: {
+                width: "150px",
+              },
+            }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            <MenuItem onClick={() => {}}>
+              {settings.PROFILE.icon}
+              <Typography fontSize="14px" ml="10px" textAlign="center">
+                {settings.PROFILE.label}
+              </Typography>
+            </MenuItem>
+            <Divider />
+            <MenuItem
+              style={{ color: "red" }}
+              onClick={() => {
+                localStorage.clear();
+                setAuth({});
+                navigate("/login");
+              }}
+            >
+              {settings.LOGOUT.icon}
+              <Typography fontSize="14px" ml="10px" textAlign="center">
+                {settings.LOGOUT.label}
+              </Typography>
+            </MenuItem>
+          </Menu>
+        </Box>
       </Toolbar>
     </AppBarStyle>
   );
