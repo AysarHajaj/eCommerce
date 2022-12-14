cd42946fca208eab0190ca4259102657e2e59447
aysar <aysarhajaj@gmail.com>
2022-12-14 22:44:48 +0400

    add products  toutes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ChildCategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\SubCategoryController;
 use App\Http\Controllers\UserController;
@@ -19,17 +20,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//authentication apis
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
 Route::middleware("auth:api")->get('logout', [AuthenticationController::class, 'logout']);
 Route::get('unauthenticated', [AuthenticationController::class, 'unauthenticated'])->name('unauthenticated');
+
+//categories apis
 Route::resource('categories', CategoryController::class);
 Route::post('categories/{id}', [CategoryController::class, 'update']);
 Route::post('categories/{id}/change_status', [CategoryController::class, 'changeStatus']);
+
+//sub categories apis
 Route::resource('sub_categories', SubCategoryController::class);
-Route::resource('child_categories', ChildCategoryController::class);
 Route::post('sub_categories/{id}/change_status', [SubCategoryController::class, 'changeStatus']);
+
+//child categories apis
+Route::resource('child_categories', ChildCategoryController::class);
 Route::post('child_categories/{id}/change_status', [ChildCategoryController::class, 'changeStatus']);
 
 //vendor apis
@@ -47,3 +54,8 @@ Route::prefix('shops')->group(function () {
     Route::get('/{id}', [ShopController::class, 'show']);
     Route::post('/{id}', [ShopController::class, 'update']);
 });
+
+//products apis
+Route::resource('products', ProductController::class);
+Route::post('products/{id}', [ProductController::class, 'update']);
+Route::post('products/{id}/change_status', [ProductController::class, 'changeStatus']);
