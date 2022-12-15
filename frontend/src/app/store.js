dc184252af574e9a5860bdb3932b825bbb6d7cb4
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2022-12-15 13:38:49 +0200

    Remove counter from store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import categoryReducer from "../features/Category/categorySlice";
 import subCategoryReducer from "../features/SubCategory/subCategorySlice";
 import childCategoryReducer from "../features/ChildCategory/childCategorySlice";
@@ -10,7 +9,6 @@ import shopReducer from "../features/Shops/shopSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     category: categoryReducer,
     sub_category: subCategoryReducer,
     child_category: childCategoryReducer,
