3caea924038995407fe6e88c3994b39444e9817b
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2022-12-24 00:42:14 +0200

    Refresh app when the user opens multiple tabs and change the authentication data

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useMemo } from 'react';
+import React, { createContext, useState, useMemo, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 const AuthContext = createContext({});
@@ -15,8 +15,22 @@ const getUserData = () => {
 
 export function AuthProvider({ children }) {
   const [auth, setAuth] = useState(getUserData);
-
   const providerValue = useMemo(() => ({ auth, setAuth }), [auth, setAuth]);
+
+  const focusEventListener = useCallback(() => {
+    const storageUser = JSON.parse(localStorage.getItem('user'));
+    if (auth?.user?.id !== storageUser?.id) {
+      window.location.reload();
+    }
+  }, [auth?.user?.id]);
+
+  useEffect(() => {
+    window.addEventListener('focus', focusEventListener);
+
+    return () => {
+      window.removeEventListener('focus', focusEventListener);
+    };
+  }, [focusEventListener]);
 
   return <AuthContext.Provider value={providerValue}>{children}</AuthContext.Provider>;
 }
