ef3715c6d68ce7d1f77ae4f7ecc2933d30af7960
aysar <aysarhajaj@gmail.com>
2022-12-31 05:42:48 +0400

    fix categories components

--- frontend/src/components/ShopCategoriesCarousel/index.js
+++ frontend/src/components/ShopCategoriesCarousel/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/self-closing-comp */
 import React from 'react';
@@ -9,63 +10,46 @@ function ShopCategoriesCarousel({ categories }) {
   const navigate = useNavigate();
 
   return (
-    <div className="shop-default-category category-ellipse-section mb-6">
-      <div
-        className="swiper-container swiper-theme shadow-swiper"
-        data-swiper-options="{
-      'spaceBetween': 20,
-      'slidesPerView': 2,
-      'breakpoints': {
-          '480': {
-              'slidesPerView': 3
-          },
-          '576': {
-              'slidesPerView': 4
-          },
-          '768': {
-              'slidesPerView': 6
-          },
-          '992': {
-              'slidesPerView': 7
-          },
-          '1200': {
-              'slidesPerView': 8,
-              'spaceBetween': 30
-          }
-      }
-  }"
-      >
-        <div
-          style={{ overflow: 'auto' }}
-          className="swiper-wrapper row gutter-lg cols-xl-8 cols-lg-7 cols-md-6 cols-sm-4 cols-xs-3 cols-2"
-        >
-          {categories.map((category) => (
-            <div key={category.id} className="swiper-slide category-wrap">
-              <div className="category category-ellipse">
-                <figure className="category-media">
-                  <a onClick={() => navigate(ROUTES.VENDORS_PAGE.dynamicPath(category.id))}>
-                    <img
-                      src={category.image}
-                      alt="Category"
-                      width="190"
-                      height="190"
-                      style={{ backgroundColor: '#5C92C0' }}
-                    />
-                  </a>
-                </figure>
-                <div className="category-content">
-                  <h4 className="category-name">
-                    <a onClick={() => navigate(ROUTES.VENDORS_PAGE.dynamicPath(category.id))}>
-                      {category.name}
-                    </a>
-                  </h4>
-                </div>
+    <section className="sections container-fluid mx-3 border-top border-bottom pt-3 pb-3">
+      <div className="row">
+        <div className="col">
+          <h2 className="title text-left pt-1 mb-5 appear-animate-visible margin-bottom-important">
+            <i className="bi bi-basket mx-3" />
+            Multi Categories
+          </h2>
+
+          <div className="swiper categories-swiper">
+            <div className="swiper-container category-wrapper swiper-theme appear-animate-visible">
+              <div className="swiper-wrapper row cols-xl-6 cols-lg-5 cols-md-4 cols-sm-3 cols-2 d-flex justify-content-center">
+                {categories.map((category) => (
+                  <div
+                    key={category.id}
+                    onClick={() => navigate(ROUTES.VENDORS_PAGE.dynamicPath(category.id))}
+                    className="swiper-slide category category-ellipse app-category margin-top-important"
+                  >
+                    <figure className="category-media">
+                      <a>
+                        <img
+                          src={category.image}
+                          alt="Category"
+                          className="category-img"
+                          style={{ backgroundColor: '#5C92C0' }}
+                        />
+                      </a>
+                    </figure>
+                    <div className="category-content">
+                      <h4 className="category-name">
+                        <a>{category.name}</a>
+                      </h4>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
